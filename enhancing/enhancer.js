c38864587fb3fa3b3655f5ccb31b5e77c5968b48
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-19 18:35:39 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -8,7 +8,6 @@ module.exports = {
 function success(item) {
 	if (item.enhancement < 20) {
 		item.enhancement = item.enhancement + 1;
-		console.log("ITEM HAS BEEN ENHANCED BY 1 POINT!");
 	} else {
 		console.log("Item cannot be enhanced any further!");
 	}
@@ -16,28 +15,37 @@ function success(item) {
 } // WOKRING
 
 function fail(item) {
-	if (item.enhancement < 15) {
+	console.log("fail log start");
+	// enhancement over 20
+	if (item.enhancement > 16) {
+		console.log("fail log above 16");
+		item.enhancement = item.enhancement - 1;
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+			console.log("dura above 10");
+		} else {
+			item.durability = item.durability - item.durability;
+			console.log("dura below 10");
+		}
+	}
+	// enhancement between 15 and 16
+	else if (item.enhancement === 15 || 16) {
+		console.log("fail log at 15 or 16");
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	}
+	// enhancement below 15
+	else if (item.enhancement < 15) {
+		console.log("fail log below 15");
 		if (item.durability > 5) {
 			item.durability = item.durability - 5;
 		} else {
 			item.durability = item.durability - item.durability;
 		}
-	} else if ((item.enhancement = 15)) {
-		if (item.durability > 10) {
-			item.durability = item.durability - 10;
-		} else {
-			item.durability = item.durability - item.durability;
-		}
-	} else if (item.enhancement >= 16) {
-		if (item.durability > 10) {
-			item.durability = item.durability - 10;
-			item.enhancement = item.enhancement - 1;
-		} else {
-			item.durability = item.durability - item.durability;
-			item.enhancement = item.enhancement - 1;
-		}
 	}
-	return { ...item };
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -4,40 +4,45 @@ const { repair, success, fail } = require("./enhancer.js");
 let item = {
 	name: "EpicLoot",
 	durability: 50,
-	enhancement: 10,
+	enhancement: 13,
 };
 
-describe("enhancing", () => {
+describe("enhancer", () => {
 	describe("repair", () => {
-		// DEFINING INFORMATION ABOUT THE ITEM == WORKING
-		it("should be an item with a name of EpicLoot durability of 50", () => {
-			expect(item.name).toBe("EpicLoot");
-		});
-		it("should be an item with durability of 50", () => {
-			expect(item.durability).toBe(50);
-		});
-		it("should be an item with enhancment of 10", () => {
-			expect(item.enhancement).toBe(10);
-		});
-
 		// TESTING THE REPAIR FUNCTION == WORKING
 		it("should have it's item durability returned to 100", () => {
 			repair(item);
 			expect(item.durability).toBe(100);
 			// expect(item.durability).toBe(50); // intentional failure
 		});
+	});
+});
 
+describe("enhancer", () => {
+	describe("success", () => {
 		// TESTING THE SUCCESS FUNCTION == WORKING
-		it("should increase durability by 1 if not currently at 20", () => {
+		it("should increase enhancement by 1 if not currently at 20", () => {
 			success(item);
-			expect(item.enhancement).toBe(11);
-			// expect(item.enhancement).toBe(18); // intentional failure
-		});
+			expect(item.enhancement).toBe(14);
 
+			success(item); // 15
+			success(item); // 16
+			success(item); // 17
+			success(item); // 18
+			success(item); // 19
+			success(item); // 20
+			expect(item.enhancement).toBe(20);
+		});
+	});
+});
+
+describe("enhancer", () => {
+	describe("failure", () => {
 		// TESTING THE FAILURE FUNCTION ==
-		it("should fail to increase item enhancement AND apply penalties to durability", () => {
-			fail(item); // where enhance = 10
-			expect(item.durability).toBe(45);
+		it("should fail and remove 1 from enhance and 10 from dura", async () => {
+			await fail(item);
+			expect(item.durability).toBe(90);
+			expect(item.enhancement).toBe(19);
 		});
 	});
 });
