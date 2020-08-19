78813b9104daaeaacfdd6872bf9818d406b01967
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-19 17:13:42 -0400

    working on the fail function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,56 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	success,
+	fail,
+	repair,
+	get,
 };
 
 function success(item) {
-  return { ...item };
-}
+	if (item.enhancement < 20) {
+		item.enhancement = item.enhancement + 1;
+		console.log("ITEM HAS BEEN ENHANCED BY 1 POINT!");
+	} else {
+		console.log("Item cannot be enhanced any further!");
+	}
+	return { ...item };
+} // WOKRING
 
 function fail(item) {
-  return { ...item };
+	if (item.enhancement < 15) {
+		if (item.durability > 5) {
+			item.durability = item.durability - 5;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	} else if ((item.enhancement = 15)) {
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+		} else {
+			item.durability = item.durability - item.durability;
+		}
+	} else if (item.enhancement >= 16) {
+		if (item.durability > 10) {
+			item.durability = item.durability - 10;
+			item.enhancement = item.enhancement - 1;
+		} else {
+			item.durability = item.durability - item.durability;
+			item.enhancement = item.enhancement - 1;
+		}
+	}
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
-}
+	if (item) {
+		console.log("ITEM REPAIR SUCCESSFUL");
+		item.durability = 100;
+	} else {
+		console.log("ITEM REPAIR FAILED!");
+		item.durability = item.durability;
+	}
+	return { ...item };
+} // WOKRING
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,43 @@
-const enhancer = require('./enhancer.js');
+const { repair, success, fail } = require("./enhancer.js");
 // test away!
+
+let item = {
+	name: "EpicLoot",
+	durability: 50,
+	enhancement: 10,
+};
+
+describe("enhancing", () => {
+	describe("repair", () => {
+		// DEFINING INFORMATION ABOUT THE ITEM == WORKING
+		it("should be an item with a name of EpicLoot durability of 50", () => {
+			expect(item.name).toBe("EpicLoot");
+		});
+		it("should be an item with durability of 50", () => {
+			expect(item.durability).toBe(50);
+		});
+		it("should be an item with enhancment of 10", () => {
+			expect(item.enhancement).toBe(10);
+		});
+
+		// TESTING THE REPAIR FUNCTION == WORKING
+		it("should have it's item durability returned to 100", () => {
+			repair(item);
+			expect(item.durability).toBe(100);
+			// expect(item.durability).toBe(50); // intentional failure
+		});
+
+		// TESTING THE SUCCESS FUNCTION == WORKING
+		it("should increase durability by 1 if not currently at 20", () => {
+			success(item);
+			expect(item.enhancement).toBe(11);
+			// expect(item.enhancement).toBe(18); // intentional failure
+		});
+
+		// TESTING THE FAILURE FUNCTION ==
+		it("should fail to increase item enhancement AND apply penalties to durability", () => {
+			fail(item); // where enhance = 10
+			expect(item.durability).toBe(45);
+		});
+	});
+});
